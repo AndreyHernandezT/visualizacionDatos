0815de7ffbc6e2af04569348b9920f6539c1a264
Andrey Hernandez <andreyhernandeztorres@gmail.com>
2022-08-13 11:42:05 -0500

    First Commit

---
+++ 1_🤓_HomePage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+def main():
+    st.title('Visualización de los datos Abalone')
+
+    st.sidebar.success("Selecciona una página")
+
+if __name__ == '__main__':
+    main()
