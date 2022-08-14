63b88e50523707d756660cf9d436df0b74d5df28
Andrey Hernandez <andreyhernandeztorres@gmail.com>
2022-08-13 20:41:49 -0500

    graph Regression

--- pages/2_📈_Graficas.py
+++ pages/2_📈_Graficas.py
@@ -1,10 +1,7 @@
 from sys import flags
 import streamlit as st
-#import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-import plotly as py
-import matplotlib.pyplot as plt 
 from statsmodels.graphics.gofplots import qqplot
 
 def remove_outlier(dataset_in, col_name, value_alpha):
@@ -68,23 +65,19 @@ def graph_two_columns(dataset, column_input, column_output, graph, tittle_graph)
     if graph == 'Dispersión':
         fig = px.scatter(dataset, x=column_input, y=column_output, title=tittle_graph)
         st.plotly_chart(fig)
-    # elif graph == "Cajas y Bigotes":
-    #     fig = px.box(dataset, x=column, title=tittle_graph)
-    #     st.plotly_chart(fig)
 
-def graph_regression(options_column, column):
-    st.warning("Recuerda que para usar **Regresión Lineal** debes incluir al menos una variable de entrada y sólo una de salida")
-    col3, col4 = st.columns(2)
-
-    inputs_selection = col3.multiselect('Variable(s) de entrada:', options_column, default=column)
-
-    column_input = ['Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings']
-    column_to_compare = col4.selectbox("Variable de salida", column_input)
+def graph_regression(dataframe, inputs_selection, column_to_compare):
+    fig = px.scatter(dataframe, x=inputs_selection[0], y=column_to_compare, trendline="ols", trendline_scope="overall", trendline_color_override="green")
+    st.plotly_chart(fig)
+    results = px.get_trendline_results(fig)
+    results = results.iloc[0]["px_fit_results"].summary()
+    st.write(results)
 
 
 
+def main():
+    st.title("Gráficas")
 
-def user_input():
     col1, col2 = st.columns(2)
 
     options_column = ['Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings']
@@ -93,11 +86,17 @@ def user_input():
     options_graph = ["Histograma", "Cajas y Bigotes", "Probabilidad Normal", "Regresion Lineal", "Dispersión"]
     graph = col2.selectbox("¿Qué gráfica quieres ver?", options_graph)
 
-    alpha_selection = None
     output_column = None
+    input_column = None
 
     if graph == "Regresion Lineal":
-        graph_regression(options_column, column)
+        st.warning("Recuerda que para usar **Regresión Lineal** debes incluir al menos una variable de entrada y sólo una de salida")
+        col3, col4 = st.columns(2)
+
+        input_column = col3.multiselect('Variable(s) de entrada:', options_column, default=column)
+
+        column_input = ['Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings']
+        output_column = col4.selectbox("Variable de salida", column_input)
     
     elif graph == 'Dispersión':
         st.warning("Recuerda que para la **Dispersión** debes incluir una variable de salida")
@@ -108,25 +107,37 @@ def user_input():
     if flag:
         alpha_selection = st.slider("Factor de alfa para los atípicos:", min_value=0.0, max_value=3.0, value=1.5)
 
-    #st.write(st.session_state["my_dataset"])
     if st.button('Generar Gráfica'):
         try:
             dataset = st.session_state["my_dataset"]
             if flag:
                 if output_column != None:
-                    title_with = "Gráfica "+graph+" CON Atipicos"
-                    graph_two_columns(dataset, column, output_column, graph, title_with)
-                    title_without  = "Gráfica "+graph+" SIN Atipicos"
-                    dataset_clean = remove_outlier(dataset, column, alpha_selection)
-                    graph_two_columns(dataset_clean, column, output_column, graph, title_without)
+                    if input_column != None:
+                        graph_regression(dataset, input_column, output_column)
+
+                        dataset_clean = remove_outlier(dataset, column, alpha_selection)
+                        graph_regression(dataset_clean, input_column, output_column)
+
+                    else:
+                        title_with = "Gráfica "+graph+" CON Atipicos"
+                        graph_two_columns(dataset, column, output_column, graph, title_with)
+
+                        title_without  = "Gráfica "+graph+" SIN Atipicos"
+                        dataset_clean = remove_outlier(dataset, column, alpha_selection)
+
+                        graph_two_columns(dataset_clean, column, output_column, graph, title_without)
+                        st.write("Se eliminaron ", (len(dataset)-len(dataset_clean)), "registros. Actualmente hay",len(dataset_clean))
                 else:
                     dataset_clean = remove_outlier(dataset, column, alpha_selection)
                     title_with = "Gráfica "+graph+" CON Atipicos"
                     generate_graphics(dataset, column, graph, title_with)
+
                     title_without  = "Gráfica "+graph+" SIN Atipicos"
                     generate_graphics(dataset_clean, column, graph, title_without)
+                    st.write("Se eliminaron ", (len(dataset)-len(dataset_clean)), "registros. Actualmente hay",len(dataset_clean))
             else:
                 if output_column != None:
+                    if input_column!=None : graph_regression(dataset, input_column, output_column)
                     title_without  = "Gráfica "+graph
                     graph_two_columns(dataset, column, output_column, graph, title_without)
                 else:
@@ -134,20 +145,6 @@ def user_input():
                     generate_graphics(dataset, column, graph, title_without)
         except:
             st.error("Primero debes ir a **HomePage** y generar el dataset 😒")
-
-           
-        
-
-        
-       
-
-
-def main():
-    st.title("Dataset Abalone con Datos Atípicos")
-
-    
-    user_input()
-
 
 if __name__ == '__main__':
     main()
